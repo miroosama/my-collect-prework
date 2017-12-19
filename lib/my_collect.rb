65a78a5482f983e79400feda96b7ec1f21fde91f
osama miro <o.miro650@gmail.com>
2017-12-19 21:38:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+def my_collect(array)
 
+  i = 0
+  result = []
 
+  while i < array.size
+    block_given?
+    result << yield(array[i])
+    i += 1
+  end
+result
+end
+
+def my_collect(array)
+
+  i = 0
+  result = []
+
+  while i < array.size
+    block_given?
+    result << yield(array[i])
+    i += 1
+  end
+result
+end
