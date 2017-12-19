e05bca5f8a9493c7281bce2fb4b76ba5ae39e6c2
osama miro <o.miro650@gmail.com>
2017-12-19 21:36:55 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,11 @@
 def my_collect(array)
+i = 0
+result = []
 
-  i = 0
-  result = []
-
-  while i < array.size
-    block_given?
-    result << yield(array[i]) 
-    i += 1
-  end
+while i < array.size
+  block_given?
+result << yeild(array[i])
+  i += 1
+end
 result
 end
