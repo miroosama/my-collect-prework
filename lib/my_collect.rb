160aff5532de1b685ec696d2c45de678b07432f1
osama miro <o.miro650@gmail.com>
2017-12-19 21:33:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
 i = 0
-my_languages = []
+result = []
 
 while i < array.length
   block_given?
-my_languages << yeild(array[i])
+my_languages < yeild(array[i])
   i += 1
 end
 my_languages
